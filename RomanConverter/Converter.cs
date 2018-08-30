5ab2dd9689e7c36a5a775882cb275ebafc02a041
LAPTOP-OAQH0G0Q\birgi <birgitte.strate@gmail.com>
2018-08-30 16:27:04 +0200

    Converts from integer to roman and vise versa

---
+++ RomanConverter/Converter.cs
@@ -0,0 +1,41 @@
+﻿namespace RomanConverter
+{
+    public class Converter
+    {
+        public Roman roman;
+
+        public Converter()
+        {
+            roman = new Roman();
+        }
+
+        public string IntToRoman(int intNumber)
+        {
+            string romanNumber = "";
+            foreach (var romanItem in roman.Numerals)
+            {
+                if (intNumber >= romanItem.Key)
+                {
+                    romanNumber = romanItem.Value + IntToRoman(intNumber - romanItem.Key);
+                    break;
+                }                 
+            }            
+            return romanNumber;
+        }
+
+        public int RomanToInt(string romanNumber)
+        {
+            foreach (var romanItem in roman.Numerals)          
+            {
+                if (romanItem.Value.Length == 1)
+                {
+                    int romanIdx = romanNumber.IndexOf(romanItem.Value);
+                    if (romanIdx > -1)
+                        return RomanToInt(romanNumber.Substring(romanIdx + 1)) +
+                               (romanIdx > 0 ? romanItem.Key - RomanToInt(romanNumber.Substring(0, romanIdx)): romanItem.Key);
+                }                
+            }
+            return 0;
+        } 
+    }
+}
---
+++ RomanConverter/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace RomanConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
